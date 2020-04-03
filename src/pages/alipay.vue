b46217c4c6471b5dc2b3bf726971da3841159034
hery080 <your@email.com>
2020-04-03 17:03:27 +0800

    封装Storage

---
+++ src/pages/alipay.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    
+    ali-pay
+   
+  </div>
+</template>
+<script>
+
+  export default{
+    name:'ali-pay',
+   
+  }
+</script>
--- src/pages/order.vue
+++ src/pages/order.vue
@@ -1,17 +1,17 @@
 <template>
   <div>
-    <nav-header></nav-header>
+    <order-header></order-header>
     <router-view></router-view>
     <nav-footer></nav-footer>
   </div>
 </template>
 <script>
-  import NavHeader from './../components/NavHeader'
+  import OrderHeader from './../components/OrderHeader'
   import NavFooter from './../components/NavFooter'
   export default{
-    name:'nav-home',
+    name:'order-home',
     components:{
-      NavHeader,
+      OrderHeader,
       NavFooter
     }
   }
